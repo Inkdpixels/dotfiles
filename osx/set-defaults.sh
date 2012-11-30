24f46cab750ff32fb9b0cd974dffc3dc7ba67ba2
Inkdpixels <inkdpixels@googlemail.com>
2012-11-30 20:49:56 +0100

    Removed developer defaults for the Appstore.app

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -398,17 +398,6 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 
 
 ###############################################################################
-# Mac App Store                                                               #
-###############################################################################
-
-# Enable the WebKit Developer Tools in the Mac App Store
-defaults write com.apple.appstore WebKitDeveloperExtras -bool true
-
-# Enable Debug Menu in the Mac App Store
-defaults write com.apple.appstore ShowDebugMenu -bool true
-
-
-###############################################################################
 # Google Chrome & Google Chrome Canary                                        #
 ###############################################################################
 
