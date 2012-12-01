09a0e8451f3663cda1ec530a69b1ed3e41435f98
Inkdpixels <inkdpixels@googlemail.com>
2012-12-01 13:41:49 +0100

    Moved the killall loop into bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -10,4 +10,12 @@ export DOTFILES=$HOME/.dotfiles
 # Copy the dotfiles to the system positions.
 for file in $DOTFILES/.{aliases,bash_profile,bash_prompt,bashrc}; do
 	cp -R "$file" $HOME
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+echo "Done. Note that some of these changes require a logout/restart to take effect. Killing the the affected processes now."
+
+for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
+  "Safari" "SystemUIServer" "Terminal" "Twitter" "iCal" "iTunes"; do
+  killall "$app" > /dev/null 2>&1
 done
