c15eb01d054b1657e77e71c3a16dc52951c3d166
Inkdpixels <inkdpixels@googlemail.com>
2012-12-01 13:43:05 +0100

    Set the bootstrap.sh file as the main loading file
    Everything should be loaded from this main file, Sublime2 setup is
    optional

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,16 +4,38 @@
 # ./bootstrap.sh
 #
 
+
+
+###############################################################################
+# Shortcuts                                                                   #
+###############################################################################
 # Set up the shortcut to this dotfiles folder
 export DOTFILES=$HOME/.dotfiles
 
-# Copy the dotfiles to the system positions.
+
+###############################################################################
+# Copy the dotfiles to the system positions                                   #
+###############################################################################
 for file in $DOTFILES/.{aliases,bash_profile,bash_prompt,bashrc}; do
 	cp -R "$file" $HOME
+done
+
+
+###############################################################################
+# Loading all other files                                                     #
+###############################################################################
+# Setting up osx defaults
+$DOTFILES/osx/./setup
+
+# Setting up Sublime Text 2 - Disabled by default (USER CHOICE)
+# $DOTFILES/sublime2/./setup
+
+
 ###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 echo "Done. Note that some of these changes require a logout/restart to take effect. Killing the the affected processes now."
+sleep 3 # Wait a bit so the user can read the message
 
 for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
   "Safari" "SystemUIServer" "Terminal" "Twitter" "iCal" "iTunes"; do
