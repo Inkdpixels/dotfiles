8f5587cdbb706fce940cc39aa8a8c718a2cdd713
Inkdpixels <inkdpixels@googlemail.com>
2013-06-15 15:14:37 +0200

    Remove a deactivated source link for the install-apps file inside bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -15,7 +15,6 @@ export DOTFILES=$HOME/.dotfiles
 # Getting the helper functions
 source ./script/lib/utils
 source ./script/lib/install-deps
-#source ./script/lib/install-apps
 
 # Symlink function
 mirrorfiles() {
@@ -41,7 +40,7 @@ mirrorfiles() {
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+# Keep-alive: update existing `sudo` time stamp until 'bootstrap.sh' has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
