763e56422025a16e9a8bc21bd58d86416e6be802
Inkdpixels <inkdpixels@googlemail.com>
2013-08-28 17:41:03 +0200

    [TASK] Remove the replacement for the git credential-helper

--- git/setup.sh
+++ git/setup.sh
@@ -4,14 +4,13 @@
 if ! [ -f git/gitconfig.symlink ]
 then
 	p_info 'Setup gitconfig'
-	git_credential='osxkeychain'
 
 	p_user ' - What is your github author name?'
 	read -e git_authorname
 	p_user ' - What is your github author email?'
 	read -e git_authoremail
 
-	sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.example > git/gitconfig.symlink
+	sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" git/gitconfig.example > git/gitconfig.symlink
 
 	p_success 'The Gitconfig-File was written successfully'
 fi
