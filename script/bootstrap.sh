b30cbf59f6ae14c1fbe24db38e65aecdd370cc90
Inkdpixels <inkdpixels@googlemail.com>
2013-08-28 12:19:31 +0200

    [TASK] Add an install script for some applications wich are listed in the bootstrap.sh script

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -10,6 +10,10 @@
 export DOTFILES=$HOME/.dotfiles
 source lib/utils;
 
+# Applications wich will be installed via 'Homebew Cask'
+# @see https://github.com/phinze/homebrew-cask/tree/master/Casks
+APPSTACK="blender dropbox firefox firefox-aurora google-chrome google-chrome-canary gmail-notifr growlnotify insync imagealpha imageoptim iterm2 keepass-x libre-office little-snitch opera skype spotify sublime-text the-unarchiver thunderbird virtualbox vlc webstorm"
+
 
 ##############################################################################################
 # Install dependencies
@@ -51,4 +55,4 @@ done
 ##############################################################################################
 # Install some Applications
 ##############################################################################################
-
+source osx/install-apps
