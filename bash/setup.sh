07a4126d887b7c6d2fc6258a8c0fe91d341a336e
Inkdpixels <inkdpixels@googlemail.com>
2013-08-29 10:42:11 +0200

    [FEATURE] Add the Terminal-Theme from @mathiasbynens and use it as default

--- bash/setup.sh
+++ bash/setup.sh
@@ -7,3 +7,6 @@ symlink "$DOTFILES/bash/bash_prompt.symlink" "$HOME/.bash_prompt"
 symlink "$DOTFILES/bash/bashrc.symlink" "$HOME/.bashrc"
 symlink "$DOTFILES/bash/exports.symlink" "$HOME/.exports"
 symlink "$DOTFILES/bash/functions.symlink" "$HOME/.functions"
+
+# Use a modified version of the Pro theme by default in Terminal.app
+open "$DOTFILES/lib/Mathias.terminal"
