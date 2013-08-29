627389a43e0c4bd6f67721e8b1c669b3c5836ff4
Inkdpixels <inkdpixels@googlemail.com>
2013-08-29 11:08:55 +0200

    [TASK] Open the xCode Command Line Tools-URL by default in safari if not installed

--- xcode/install.sh
+++ xcode/install.sh
@@ -4,6 +4,7 @@
 if ! type_exists 'gcc'; then
 	p_fail "The XCode Command Line Tools must be installed first.
 	 https://developer.apple.com/downloads"
+	 open -a Safari https://developer.apple.com/downloads
 else
 	p_success "The XCode Command Line Tools are installed."
 fi
