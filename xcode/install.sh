b9c49f59b27c7261b8db768dd62da72d91e5beb6
Tyll Weiß <inkdpixels@gmail.com>
2013-12-12 18:14:10 +0100

    [FEATURE] Use the internal install method in 10.9 for the xCode CLT

--- xcode/install.sh
+++ xcode/install.sh
@@ -4,7 +4,7 @@
 if ! type_exists 'gcc'; then
 	p_fail "The XCode Command Line Tools must be installed first.
 	 https://developer.apple.com/downloads"
-	 open -a Safari https://developer.apple.com/downloads
+	 xcode-select --install
 else
 	p_success "The XCode Command Line Tools are installed."
 fi
