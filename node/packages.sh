b7be24a3e176ac42b30c815664c83f047a7e8768
Tyll Weiß <inkdpixels@googlemail.com>
2013-12-13 11:33:22 +0100

    [FEATURE] Add an Nodepackages variable in the 'config.sh.example'

--- node/packages.sh
+++ node/packages.sh
@@ -1,23 +1,6 @@
 #!/usr/bin/env bash
 
-# Check if the Grunt-CLI is already installed
-if ! type_exists 'grunt'; then
-	p_info "Grunt is not installed."
-	npm install -g grunt-cli
-	if ! type_exists 'grunt'; then
-		p_fail "Something went wrong..."
-	fi
-else
-	p_success "Grunt is installed."
-fi
 
-# Check if Bower is already installed
-if ! type_exists 'bower'; then
-	p_info "Bower is not installed."
-	npm install -g bower
-	if ! type_exists 'bower'; then
-		p_fail "Something went wrong..."
-	fi
-else
-	p_success "Bower is installed."
-fi
+for package in $NODEPACKAGES; do
+	npm install -g $package
+done
