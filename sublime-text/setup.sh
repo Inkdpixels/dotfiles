e96686a242d0ea0bec1ad12db8159c0ee0830883
Tyll Weiß <weiss@sitegeist.de>
2014-01-02 16:04:55 +0100

    [TASK] Upgrade from Sublime Text 2 to Sublime Text 3

--- sublime-text/setup.sh
+++ sublime-text/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Setup symlink for Sublime Text 2 settings
-sublime_dir=$HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
+# Setup symlink for Sublime Text 3 settings
+sublime_dir=$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages
 symlink "$DOTFILES/sublime-text/User" "$sublime_dir"
 
 # Grab the FreshCut theme
@@ -9,4 +9,4 @@ git clone https://github.com/daylerees/colour-schemes.git "$sublime_dir/Dayle Re
 
 # Symlinking the "subl" terminal shortcut
 mkdir ~/bin
-ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
